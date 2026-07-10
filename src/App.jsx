45db6a1674be0f09291f8fa8ba5e554c84278277
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-07-10 13:12:13 +0530

    feat: added tools page and dev tools to it

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Routes, Route } from "react-router-dom";
 import NavBar from './components/NavBar';
 import Home from './components/Home.jsx';
+import Tools from './components/Tools.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Tools" element={<Tools />} />
       </Routes>
     </>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,54 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function NavBar() {
     return (
         <div className="flex justify-between items-center px-5 bg-surface border-b-2 border-b-border">
 
-            <Link to="/" className="flex items-center ">
-                <div>
-                    <img src="/LogoDevDock.png" alt="" className="w-auto h-15" />
-                </div>
+            <Link to="/" className="flex items-center">
+                <img src="/LogoDevDock.png" alt="" className="w-auto h-15" />
+
                 <div className="text-md font-semibold">
                     <h1 className="text-xl font-bold text-white">
                         DevDock
                     </h1>
                 </div>
             </Link>
+
             <ul className="flex gap-8">
-                <li>Tools</li>
-                <li>About</li>
+                <li>
+                    <NavLink
+                        to="/tools"
+                        className={({ isActive }) =>
+                            isActive
+                                ? "text-white border-b border-b-primary-500"
+                                : "text-gray-300 hover:text-white"
+                        }
+                    >
+                        Tools
+                    </NavLink>
+                </li>
+
+                {/* <li>
+                    <NavLink
+                        to="/about"
+                        className={({ isActive }) =>
+                            isActive
+                                ? "text-blue-500 font-semibold"
+                                : "text-gray-300 hover:text-white"
+                        }
+                    >
+                        About
+                    </NavLink>
+                </li> */}
             </ul>
+
             <div className="flex gap-6">
-                <input type="text" placeholder="Search" className="bg-surface-2 px-2 py-1 rounded-sm" />
-                <p>
-                    GitHub
-                </p>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="bg-surface-2 px-2 py-1 rounded-sm"
+                />
+                <p>GitHub</p>
             </div>
         </div>
     );
